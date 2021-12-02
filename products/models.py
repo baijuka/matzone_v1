51af163723a1b2a9642690c0d071091870fac1b9
baijuka <baijuka@yahoo.com>
2021-12-02 22:20:32 +0000

    Finished add product functionality

--- products/models.py
+++ products/models.py
@@ -31,6 +31,7 @@ class ProductVariation(models.Model):
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     product_size = models.CharField(max_length=100, null=True, blank=True)
     product_price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField(default=True)
 
     def __str__(self):
         return self.product.name +' ('+self.product_size+')'
