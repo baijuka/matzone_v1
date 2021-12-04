45292e073e438dd6f961360eedfba4161584c267
baijuka <baijuka@yahoo.com>
2021-12-04 11:04:03 +0000

    Added 404 and 500 templates

--- matzone/settings.py
+++ matzone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mp7di%&fdzjr+_%)!_s8b27j+91pjk-5+lt!&3hdyk1&ha!sb%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = "DEVELOPMENT" in os.environ
 DEBUG = True
 
 ALLOWED_HOSTS = []
