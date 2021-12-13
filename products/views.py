fe1e6dde830fdc302dcd07a0bca706f6a00192ec
baijuka <baijuka@yahoo.com>
2021-12-13 22:11:29 +0000

    Update product views

--- products/views.py
+++ products/views.py
@@ -9,30 +9,29 @@ from .forms import ProductForm
 
 
 def all_products(request):
-    """ view to show all products, including sorting and search queries """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
     categories = None
     sort = None
     direction = None
-    
-    if 'sort' in request.GET:
-        sortkey = request.GET['sort']
-        sort = sortkey
 
-        if sortkey == 'name':
-            sortkey = 'lower_name'
-            products = products.annotate(lower_name=Lower('name'))
-        if sortkey == 'category':
-            sortkey = 'category__name'
-        if 'direction' in request.GET:
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-        products = products.order_by(sortkey)
-
-    if request.GET:
+            products = products.order_by(sortkey)
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -57,6 +56,7 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
 
 
 def product_detail(request, product_id):
