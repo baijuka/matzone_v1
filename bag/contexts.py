04723c081c5e61bd5a40f691b19dc041215d058a
baijuka <baijuka@yahoo.com>
2021-12-13 21:59:57 +0000

    Updated products page

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.models import Product
+from products.models import Product, ProductVariation
 
 def bag_contents(request):
 
@@ -10,10 +10,11 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
+    print('bag items',bag.items())
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
+            total += item_data * product.baseprice
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
@@ -23,13 +24,16 @@ def bag_contents(request):
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
+                product_variation = list(ProductVariation.objects.filter(product=product, size=size))[0]
+                print ('product variaton in bag today', product_variation.price)
+                total += quantity * product_variation.price
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
                     'product': product,
                     'size': size,
+                    'price': product_variation.price
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
