1f930889d4606731e73cd59fc18bb15ff91b9ba8
baijuka <baijuka@yahoo.com>
2021-12-14 19:03:55 +0000

    Javascript updated in product detail page

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product, ProductVariation
 
+
 def bag_contents(request):
 
     bag_items = []
@@ -10,7 +11,7 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    print('bag items',bag.items())
+    print('bag items', bag.items())
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
@@ -24,8 +25,9 @@ def bag_contents(request):
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                product_variation = list(ProductVariation.objects.filter(product=product, size=size))[0]
-                print ('product variaton in bag today', product_variation.price)
+                product_variation = list(ProductVariation.objects.filter(
+                    product=product, size=size))[0]
+                print('product variaton in bag today', product_variation.price)
                 total += quantity * product_variation.price
                 product_count += quantity
                 bag_items.append({
