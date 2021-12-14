5cf83fd70e75ca0330ab090c3830fc5740731874
baijuka <baijuka@yahoo.com>
2021-12-14 00:35:46 +0000

    Prodcut details template and bag views updated

--- static/js/price.js
+++ static/js/price.js
@@ -1,17 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     
     document.getElementById('id_product_size').addEventListener('change', function(){
-<<<<<<< HEAD
         document.getElementById('p_price').innerText = '£'+this.value;
     });
 });
-=======
-        console.log('Current value ' + this.value);
-        document.getElementById('p_price').innerText = '£'+this.value;
-    });
-
-
-
-});
-  
->>>>>>> a6659350daad7aa977d1ffee7caff789a1ffc7cc
