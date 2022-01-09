eef89b5dc8d6c6824cf47ef9dbad63a3ccd78601
baijuka <baijuka@yahoo.com>
2022-01-09 10:01:21 +0000

    Updated review function

--- products/views.py
+++ products/views.py
@@ -178,31 +178,36 @@ def delete_product(request, product_id):
 def add_review(request, product_id):
     url = request.META.get('HTTP_REFERER')
 
-    if request.method == 'POST':
-        try:
-            reviews = Review.objects.get(user_profile__id=request.user.id, product__id=product_id)
-            form = ReviewForm(request.POST, instance=reviews)
-            form.save()
-            messages.success(request, 'Thank you! Your review has been updated.')
-            return redirect(reverse('products'))
-        except Review.DoesNotExist:
-            form = ReviewForm(request.POST)
-            if form.is_valid():
-                data = Review()
-                data.subject = form.cleaned_data['subject']
-                data.rating = form.cleaned_data['rating']
-                data.review = form.cleaned_data['review']
-                data.product_id = product_id
-                data.user_profile_id = request.user.id
-                data.save()
-                messages.success(request, 'Thank you! Your review has been submitted.')
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            try:
+                reviews = Review.objects.get(user_profile__id=request.user.id, product__id=product_id)
+                form = ReviewForm(request.POST, instance=reviews)
+                form.save()
+                messages.success(request, 'Thank you! Your review has been updated.')
                 return redirect(reverse('products'))
+            except Review.DoesNotExist:
+                form = ReviewForm(request.POST)
+                if form.is_valid():
+                    data = Review()
+                    data.subject = form.cleaned_data['subject']
+                    data.rating = form.cleaned_data['rating']
+                    data.review = form.cleaned_data['review']
+                    data.product_id = product_id
+                    data.user_profile_id = request.user.id
+                    data.save()
+                    messages.success(request, 'Thank you! Your review has been submitted.')
+                    return redirect(reverse('products'))
+        else:
+            template = 'products/add_review.html'
+            product = get_object_or_404(Product, id=product_id)
+            context = {
+                'product': product,
+            }
+        
+        return render(request, template, context)
     else:
-        template = 'products/add_review.html'
-        product = get_object_or_404(Product, id=product_id)
-        context = {
-            'product': product,
-        }
-    
-    return render(request, template, context)
-    
+        messages.error(request, 'Sorry, only logged in users can \
+            leave a review.')
+        return redirect(reverse('login'))
+        
